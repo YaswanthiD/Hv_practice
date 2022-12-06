ea7bdeb86019ec7a6dd71259ef85f767c502f647
YaswanthiD <yaswanthidwaramapudi@gmail.com>
2022-12-06 11:34:57 +0530

    First commit

---
+++ hv1.py
@@ -0,0 +1,29 @@
+"""noOfCars=0
+noOfCars=0
+total_sum=0
+cars=int(input())
+bikes=int(input())
+amount_cars=cars*40
+amount_bikes=bikes*20
+total_sum=amount_cars+amount_bikes
+print("The total cars parked", cars)
+print("The total bikes parked",bikes)
+print("The amount for cars parked", amount_cars)
+print("The amount for bikes parked", amount_bikes)
+print("The total amount for both cars and bikes is : ",total_sum)"""
+# another ques 
+apples = int(input("Enter how many apples did the buyer want "))
+if(apples<0):
+    print("Please enter a positive number")
+kiwi = int(input("Enter how many kiwi did the buyer want "))
+grapes = int(input("Enter how many grapes did the buyer want "))
+
+amountForApples=apples*40
+amountForKiwi=kiwi*50
+amountForGrapes=grapes*60
+
+totalAmount = amountForApples+amountForKiwi+amountForGrapes
+print("The total amout should be paid by the user is: ", totalAmount)
+#hello all
+
+
